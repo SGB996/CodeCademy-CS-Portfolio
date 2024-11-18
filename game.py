764d2e8934d46d08f21ecbcd4b93b9f06816cf83
SGB996 <sgranda996@gmail.com>
2024-11-18 12:54:38 -0500

    added playing logic

--- game.py
+++ game.py
@@ -2,7 +2,48 @@ import random
 
 cards = {'A': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10}
 
-index = random.choice(list(cards.keys()))
-print(index)
-print(cards[index])
+# index = random.choice(list(cards.keys()))
+# print(index)
+# print(cards[index])
+
+
+player = input('Enter player name: ')
+
+def blackjack():
+    hand = {}
+    while len(hand) < 2:
+        index = random.choice(list(cards.keys()))
+        hand.update({index: cards[index]})
+    print('Your hand is: ', list(hand.keys()))
+    print('Your hand sum is: ', sum(list(hand.values())))
+    play = input('Do you want to keep playing? Y/N: ')
+    if play == 'Y':
+        index = random.choice(list(cards.keys()))
+        hand.update({index: cards[index]})
+        suma = sum(list(hand.values()))
+        if suma > 21:
+            print('You lost your sum is :', suma)
+        elif suma < 21:
+            print('Your hand sum is: ', suma)
+        print(hand)
+    newSuma = sum(list(hand.values()))
+    if newSuma > 21:
+        print('You lost your sum is :', newSuma)
+    if newSuma < 21:
+        play = input('Do you want to keep playing? Y/N: ')
+        if play == 'Y':
+            index = random.choice(list(cards.keys()))
+            hand.update({index: cards[index]})
+            newSuma = sum(list(hand.values()))
+            print('Your hand sum is: ', newSuma)
+        else: 
+            print('You stopped and your hand sum is: ', newSuma)
+
+
+
+
+
+if player != '':
+    print(blackjack())
+
 
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 This program is a blackjack game.
-In the terminal when it's initialized it will call the first bet, then it'll show the first two cards of the player, then it'll ask if the player want's to ask for another card or stop.
+In the terminal when it's initialized it will call the player name, if the player doesn't enter a name the game won't start, when the game get's a player name then it'll show the first two cards of the player, then it'll ask if the player want's to ask for another card or stop.
 If the player sums 21 then he wins if the house's sum is lower, if the player's sum is shorter that 21 and the house has a greater sum the house wins, if the player surpasses 21 automatically losses.
 The game will consist of 1 player and the house.
